b082d03d13149283781f092cbb5f138d7f64005a
Faisal Naved <faisalnaved8@gmail.com>
2019-03-04 22:21:37 +0530

    Added code for user's data

---
+++ BasketBallSwish/Assets/Scripts/UserData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class UserData {
+
+    public string userName;
+    public int age;
+    public string gender;
+    public long baskyCoins;
+
+    public UserData(string userName, int age, string gender, long baskyCoins)
+    {
+        this.userName = userName;
+        this.age = age;
+        this.gender = gender;
+        this.baskyCoins = baskyCoins;
+    }
+}
---
+++ BasketBallSwish/Assets/Scripts/UserDataController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class UserDataController : MonoBehaviour {
+
+    public UserData userData;
+    public bool uncheckedData;
+    private string gameDataProjectFilePath = "/StreamingAssets/userData.json";
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        LoadGameData();
+        uncheckedData = false;
+    }
+
+    public UserData UserData
+    {
+        get
+        {
+            return userData;
+        }
+
+        set
+        {
+            userData = value;
+        }
+    }
+
+    //fetch the data from JSON file
+    public void LoadGameData()
+    {
+        string filePath = Application.dataPath + gameDataProjectFilePath;
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            userData = JsonUtility.FromJson<UserData>(dataAsJson);
+            uncheckedData = false;
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+
+    //save the data from JSON file
+    public void SaveGameData()
+    {
+        string dataAsJson = JsonUtility.ToJson(userData, true);
+
+        string filePath = Application.dataPath + gameDataProjectFilePath;
+        File.WriteAllText(filePath, dataAsJson);
+        uncheckedData = true;
+    }
+}

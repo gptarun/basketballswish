5aced63600c2ae0c310683383f9d0a81aebdfc10
tarung1201 <tarung1201@gmail.com>
2019-03-08 19:54:40 +0530

    Added team buy option.

---
+++ BasketBallSwish/Assets/Scripts/BuyTeam.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyTeam : MonoBehaviour {
+
+    /// <summary>
+    /// In this script we are goin to buy the team based on baskey coins
+    /// </summary>
+    public Button buyButton;        // Need to set interactable true/false based on amount
+    public Button teamUp;
+    public Button teamDown;	
+
+    public void ConfirmedBuyTeam()
+    {
+        Debug.Log("Successfully bought this team");
+        // Need to check amount then allow player to buy team    
+    }
+
+    public void GoUp()
+    {
+        Debug.Log("Up");
+        //Going up
+    }
+
+    public void GoDown()
+    {
+        Debug.Log("Down");
+        //Going down
+    }
+}
